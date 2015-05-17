8099ac82517d432d76e9ebe6a293ca9b63a70700
Nathaniel Clapp <github@nattyclapp.com>
2015-05-17 15:32:52 -0500

    flesh out Article#create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+    # (this is a nice easy way to test, just prints params to screen)
+
+    @article = Article.new(article_params)
+    # @article = Article.new(params.require(:article).permit(:title, :text)) #for security ("strong parameters").
+    # Refactored into private method below, and method called in line above (article_params replaced `params[:article]`).
+
+    @article.save
+    redirect_to @article # redirecting to `show` action
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end#ArticlesController class
