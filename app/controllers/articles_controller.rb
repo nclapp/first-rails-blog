434dbb544c24677999dc1ce6eba34be6e558fa7e
Nathaniel Clapp <github@nattyclapp.com>
2015-05-17 15:58:35 -0500

    add error handling to new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,8 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
+    # added this after adding if save? functionality
   end
 
   def create
@@ -23,8 +24,14 @@ class ArticlesController < ApplicationController
     # @article = Article.new(params.require(:article).permit(:title, :text)) #for security ("strong parameters").
     # Refactored into private method below, and method called in line above (article_params replaced `params[:article]`).
 
-    @article.save
-    redirect_to @article # redirecting to `show` action
+    if @article.save
+      redirect_to @article
+      # redirecting to `show` action
+    else
+      render 'new'
+      # goes to #new action above if it's unable to save. Render is in same request, as opposed to redirect_to which issues a new request.
+    end
+
   end
 
   private
