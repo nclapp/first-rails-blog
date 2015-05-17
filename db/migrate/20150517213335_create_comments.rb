3690bd92fa88c59bcd562546a879f638f8935755
Nathaniel Clapp <github@nattyclapp.com>
2015-05-17 16:36:34 -0500

    run comments migration

--- db/migrate/20150517213335_create_comments.rb
+++ db/migrate/20150517213335_create_comments.rb
@@ -3,9 +3,27 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :article, index: true, foreign_key: true
+      t.references :article, index: true, foreign_key: true #adds 'article_id'
 
       t.timestamps null: false
     end
   end
 end
+
+
+# class CreateComments < ActiveRecord::Migration
+#   def change
+#     create_table :comments do |t|
+#       t.string :commenter
+#       t.text :body
+
+#       # this line adds an integer column called `article_id`.
+#       t.references :article, index: true
+
+#       t.timestamps null: false
+#     end
+#     add_foreign_key :comments, :articles
+#   end
+# end
+
+# Tutorial expected above, very slightly different with foreign key assignments
