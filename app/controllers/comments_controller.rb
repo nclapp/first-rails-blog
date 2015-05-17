9ef712b354fab76c82a15567715d5543f33919ff
Nathaniel Clapp <github@nattyclapp.com>
2015-05-17 16:45:04 -0500

    add copy/pasted comment to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,3 +13,8 @@ class CommentsController < ApplicationController
   end
 
 end
+
+
+# You'll see a bit more complexity here than you did in the controller for articles. That's a side-effect of the nesting that you've set up. Each request for a comment has to keep track of the article to which the comment is attached, thus the initial call to the find method of the Article model to get the article in question.
+
+# In addition, the code takes advantage of some of the methods available for an association. We use the create method on @article.comments to create and save the comment. This will automatically link the comment so that it belongs to that particular article.
